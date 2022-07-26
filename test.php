7582ec59f6631d52de22f1fbb1ef52fe83c0855b
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-07-26 16:13:41 +0430

    php arrays (numeric & associative) has been added...

---
+++ test.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>home</title>
+</head>
+<body>
+    
+      <center><h1>Welcome to Sama.edu.af</h1></center>
+      <hr>
+      <hr>
+      
+</body>
+</html>
+
+<?php 
+
+   
+         $name = "Ahmad";
+         $name = "Ali";
+         $name = "Maryam";
+
+
+        // echo $name;
+
+   /*
+
+         $customers = array("Ali","Ahmad","Fatima","Maryam","Faisal","Farmanullah","Mhamood");  // Numeric array...
+
+
+              for($i = 0; $i<count($customers);$i++)
+                   echo $customers[$i]."<br/>";
+
+                   
+
+
+          $custmers = array(
+                              array("Ali","Ahmad","Fawad"),
+                              array("Fatima","Maryam", "Layla"),
+                              array("Faisal", "Sana", "Kamal")
+                          );
+
+            echo $custmers[1][2];
+
+               
+
+            $names = array('name'=> "Ali", 'lname'=> "Naseri", 'email'=> "ali@yahoo.com");
+
+
+                   echo $names['email'];
+
+                   
+
+            $names = array(
+
+                     "Male"=>array("Ali", "Ahmad","Faisal","Farhad"),
+                     "Female"=>array("Maryam", "Fatima", "Layla","Noda")
+
+            );
+
+                echo $names['Male'][3];
+
+
+                */
+
+
+
+
+
+?>
